8c2dde89aefe9f8de7254b4c0fafb36c70e01da0
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-02-14 23:52:16 -0800

    Update wallet.py

--- m0rkcoin_tip_bot/wallet.py
+++ m0rkcoin_tip_bot/wallet.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 
-from m0rkcoin_tip_bot import rpc_client
-from m0rkcoin_tip_bot.config import config
+from rhodiumcoin_tip_bot import rpc_client
+from rhodiumcoin_tip_bot.config import config
 
 
 def register() -> str:
